a0db511cf6cbd7e21a9559dd7221043960ec212b
Mihaeru <gamzeescript@outlook.com>
2022-12-03 22:20:16 -0600

    add routes
    se agregan lass otras rutas logicas

--- finanzas/routes/web.php
+++ finanzas/routes/web.php
@@ -22,11 +22,32 @@ Route::get('/entradas', [EntradasController::class, 'index'])->name('entradas');
 
 Route::post('/entradas', [EntradasController::class, 'store'])->name('entradas');
 
-Route::get('/entradas/{$id}', [EntradasController::class, 'show'])->name('entradas-edit');
+Route::get('/entradas/{$id}', [EntradasController::class, 'show'])->name('entradas-show');
 
 Route::patch('/entradas/{$id}', [EntradasController::class, 'update'])->name('entradas-update');
 
 Route::delete('/entradas/{$id}', [EntradasController::class, 'destroy'])->name('entradas-destroy');
+
+Route::get('/salidas', [SalidasController::class, 'index'])->name('salidas');
+
+Route::post('/salidas', [SalidasController::class, 'store'])->name('salidas');
+
+Route::get('/salidas/{$id}', [SalidasController::class, 'show'])->name('salidas-show');
+
+Route::patch('/salidas/{$id}', [SalidasController::class, 'update'])->name('salidas-update');
+
+Route::delete('/salidas/{$id}', [SalidasController::class, 'destroy'])->name('salidas-destroy');
+
+Route::get('/entradas-detalle', [EntradasDetallesController::class, 'index'])->name('entradasdetalle');
+
+Route::get('/entradas-detalle/{$id}', [EntradasDetallesController::class, 'show'])->name('entradasdetalle-show');
+
+Route::get('/salidas-detalle', [SalidasDetallesController::class, 'index'])->name('salidas');
+
+Route::get('/salidas-detalle/{$id}', [SalidasDetallesController::class, 'show'])->name('salidasdetalle-show');
+
+
+
 
 
 
